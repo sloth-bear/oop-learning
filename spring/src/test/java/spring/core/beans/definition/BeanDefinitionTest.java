fa677198ed5d0ab7e15b53580d0d255a17d4bc0a
sloth-bear <slothbear.hj@gmail.com>
2022-07-13 23:40:45 +0900

    Add bean definition metadata
    - 실제 테스트 코드는 아니고, metadata 확인하는 용도로 사용

---
+++ spring/src/test/java/spring/core/beans/definition/BeanDefinitionTest.java
@@ -0,0 +1,27 @@
+package spring.core.beans.definition;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.core.AppConfig;
+
+public class BeanDefinitionTest {
+
+  AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+  @Test
+  @DisplayName("빈 설정 메타정보 확인할 수 있다.")
+  void beanMetadata() {
+    final var beanDefinitionNames = ac.getBeanDefinitionNames();
+
+    for (final var name : beanDefinitionNames) {
+      final var definition = ac.getBeanDefinition(name);
+
+      if (definition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+        System.out.println("beanDefinitionName = " + name + ", metadata = " + definition);
+      }
+
+    }
+  }
+}
