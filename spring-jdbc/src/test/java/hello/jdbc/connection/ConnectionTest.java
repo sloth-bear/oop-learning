006679f97fd422a53fcb02549797fcffd01565c4
sloth-bear <slothbear.hj@gmail.com>
2022-08-18 21:07:04 +0900

    Add database connection pool example tests

--- spring-jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ spring-jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -3,12 +3,17 @@ package hello.jdbc.connection;
 import static hello.jdbc.connection.ConnectionConst.PASSWORD;
 import static hello.jdbc.connection.ConnectionConst.URL;
 import static hello.jdbc.connection.ConnectionConst.USERNAME;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.SQLTransientConnectionException;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -35,5 +40,47 @@ public class ConnectionTest {
     log.info("connection = {}, class = {}", conn2, conn2.getClass());
     assertThat(conn1).isNotSameAs(conn2);
   }
-  
+
+  @Test
+  @SuppressWarnings("resource")
+  @DisplayName("Connection pooling")
+  void dataSourceConnectionPool() throws SQLException, InterruptedException {
+    final HikariDataSource dataSource = new HikariDataSource();
+    dataSource.setJdbcUrl(URL);
+    dataSource.setUsername(USERNAME);
+    dataSource.setPassword(PASSWORD);
+    dataSource.setMaximumPoolSize(10);
+    dataSource.setPoolName("MyPool");
+
+    final var conn1 = dataSource.getConnection();
+    final var conn2 = dataSource.getConnection();
+
+    log.info("connection = {}, class = {}", conn1, conn1.getClass());
+    log.info("connection = {}, class = {}", conn2, conn2.getClass());
+
+    Thread.sleep(1000); // connection 생성 시간 대기
+
+    assertThat(conn1).isNotSameAs(conn2);
+  }
+
+  @SuppressWarnings("resource")
+  @Test
+  @DisplayName("should block when connection requests exceed than connection pool max size")
+  void dataSourceConnectionPoolExceed() {
+    final HikariDataSource dataSource = new HikariDataSource();
+    dataSource.setJdbcUrl(URL);
+    dataSource.setUsername(USERNAME);
+    dataSource.setPassword(PASSWORD);
+    dataSource.setMaximumPoolSize(10);
+    dataSource.setPoolName("MyPool");
+    dataSource.setConnectionTimeout(SECONDS.toMillis(10));
+
+    // after 10 seconds (max lifetime)
+    assertThatThrownBy(() -> {
+      for (int i = 0; i < 11; i++) {
+        dataSource.getConnection();
+      }
+    }).isInstanceOf(SQLTransientConnectionException.class);
+  }
+
 }
