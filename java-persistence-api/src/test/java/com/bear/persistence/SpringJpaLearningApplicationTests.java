929d3fc432665d0e6eae461edd4077120e44bab2
sloth-bear <slothbear.hj@gmail.com>
2023-01-10 10:16:01 +0900

    Add java-persistence-api practice project

---
+++ java-persistence-api/src/test/java/com/bear/persistence/SpringJpaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bear.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
