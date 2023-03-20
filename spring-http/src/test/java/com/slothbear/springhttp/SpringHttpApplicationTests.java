22b8660f6506cf38e125724ce650e566ced2019d
sloth-bear <slothbear.hj@gmail.com>
2023-03-20 21:17:01 +0900

    Init spring-http project

---
+++ spring-http/src/test/java/com/slothbear/springhttp/SpringHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slothbear.springhttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
